57c6dcda4d621de901384e92a69f3139ad04a25e
vargasraymondj <vargasraymondj@gmail.com>
2021-10-27 11:45:23 -0400

    Brew formula update for awsmsauce version v0.11.0

--- Formula/awsmsauce.rb
+++ Formula/awsmsauce.rb
@@ -5,20 +5,20 @@
 class Awsmsauce < Formula
   desc "Example binary distribution using goreleaser and homebrew"
   homepage "https://github.com/VargasRaymondJ/homebrew-jackalope-test"
-  version "0.8.0"
+  version "0.11.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/VargasRaymondJ/awsmsauce/releases/download/v0.8.0/awsmsauce_0.8.0_Darwin_arm64.tar.gz"
-      sha256 "8f0736dc646670840705be7d218a1d73d5916807e8ae2561ac489f1589a19a76"
+      url "https://github.com/VargasRaymondJ/awsmsauce/releases/download/v0.11.0/awsmsauce_0.11.0_Darwin_arm64.tar.gz"
+      sha256 "b90cd8038b0c264f44bf358695e88cab0265f4a4ff8ede3306be47512aa9b4da"
 
       def install
         bin.install "awsmsausage"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/VargasRaymondJ/awsmsauce/releases/download/v0.8.0/awsmsauce_0.8.0_Darwin_x86_64.tar.gz"
-      sha256 "675f40a54ce3a4815d047fb508e8fd6bd7996c84be20ab36371df26b687e86aa"
+      url "https://github.com/VargasRaymondJ/awsmsauce/releases/download/v0.11.0/awsmsauce_0.11.0_Darwin_x86_64.tar.gz"
+      sha256 "24057d1df6b2f69105047be1d99e95a9cadb047850a7bc661702ae2e1411a168"
 
       def install
         bin.install "awsmsausage"
@@ -28,16 +28,16 @@ class Awsmsauce < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/VargasRaymondJ/awsmsauce/releases/download/v0.8.0/awsmsauce_0.8.0_Linux_arm64.tar.gz"
-      sha256 "48cd2167e7259c361bbaac7a45fd515b10d8cad2f3d2260be67817bef89e7858"
+      url "https://github.com/VargasRaymondJ/awsmsauce/releases/download/v0.11.0/awsmsauce_0.11.0_Linux_arm64.tar.gz"
+      sha256 "0788d49fbdfc88226747c3a56332d4a6206ada152e5349b29078da9545bf90c7"
 
       def install
         bin.install "awsmsausage"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/VargasRaymondJ/awsmsauce/releases/download/v0.8.0/awsmsauce_0.8.0_Linux_x86_64.tar.gz"
-      sha256 "a70540588d79da09e3567f3ab2967e43e3d8f5db7476821e150b81599c8bdc76"
+      url "https://github.com/VargasRaymondJ/awsmsauce/releases/download/v0.11.0/awsmsauce_0.11.0_Linux_x86_64.tar.gz"
+      sha256 "3c210b3fb9aca93f9551475e8d53a8a1c171c306c602ed19679dab16a308b9b8"
 
       def install
         bin.install "awsmsausage"
